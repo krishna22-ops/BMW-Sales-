d64c3a4d5fb2c3c2b70881f2b4d914eb8a53dfdb
Krishna Tamang <krishnatmg482@gmail.com>
2025-07-21 19:04:20 +0100

    Add files via upload

---
+++ Insert Into.sql
@@ -0,0 +1,49 @@
+-- Insert into dealership table --
+insert into dealership
+values (1, 'Auto Galaxy', 'New York, USA', '212-555-0101'),
+		(2, 'BMW Premier', 'Los Angeles, USA', '310-555-0123'),
+        (3, 'Luxury Wheels', 'Chicago, USA', '773-555-0145'),
+        (4, 'DriveTime Motors', 'Houston, USA', '713-555-0167'),
+        (5, 'Ultimate Autos', 'Miami, USA', '305-555-0189'),
+        (6, 'City Cars', 'San Francisco, USA', '415-555-0202'),
+        (7, 'Speedline Autos', 'Seattle, USA', '206-555-0224'),
+        (8, 'Premier Motors', 'Boston, USA', '617-555-0246'),
+        (9, 'Elite Cars', 'Atlanta, USA', '404-555-0268'),
+        (10, 'Grand Auto Sales', 'Denver, USA', '303-555-0280');
+
+
+-- Insert into bmw_sales --
+insert into bmw_sales
+values 
+(100,7,'5 Series',2016,3.5,98740,151748,'Petrol','Manual','Red'),
+(101,3,'i8',2013,1.6,79219,121671,'Hybrid','Automatic','Red'),
+(102,9,'5 series',2022,4.5,113265,10991,'Petrol','Automatic','Blue'),
+(103,2,'X3',2024,1.7,60971,27255,'Petrol','Automatic','Blue'),
+(104,5,'7 Series',2020,2.1,49898,122131,'Diesel','Manual','Black'),
+(105,1,'5 Series',2017,1.9,42926,171362,'Diesel','Manual','Silver'),
+(106,6,'i8',2022,1.8,55064,196741,'Diesel','Manual','White'),
+(107,10,'M5',2014,1.6,102778,121156,'Diesel','Automatic','Black'),
+(108,2,'X3',2016,1.7,116482,48073,'Diesel','Automatic','White'),
+(109,8,'i8',2019,3.0,96257,35700,'Electric','Manual','White'),
+(110,3,'3 Series',2012,2.4,117995,101595,'Electric','Manual','White'),
+(111,5,'i8',2016,1.9,74539,116863,'Hybrid','Manual','Blue'),
+(112,7,'7 Series',2020,3.8,100015,163444,'Diesel','Automatic','Silver'),
+(113,10,'5 Series',2020,2.3,119486,163444,'Electric','Manual','White'),
+(114,4,'X1',2017,1.6,34078,107918,'Electric','Automatic','Black'),
+(115,8,'M3',2014,3.0,94997,154050,'Hybrid','Manual','Silver'),
+(116,9,'X5',2013,2.6,114844,110142,'Electric','Manual','Blue'),
+(117,1,'M5',2017,2.6,81934,86900,'Diesel','Automatic','Red'),
+(118,6,'5 Series',2017,3.1,111326,57843,'Electric','Manual','Silver'),
+(119,4,'X5',2012,3.8,82677,142243,'Electric','Manual','Blue'),
+(120,2,'X3',2015,3.7,88091,123910,'Diesel','Automatic','Red'),
+(121,3,'X1',2014,4.8,62941,13568,'Petrol','Manual','White'),
+(122,5,'M5',2011,3.3,49507,78042,'Electric','Automatic','Grey'),
+(123,9,'7 Series',2017,3.2,58288,101791,'Petrol','Automatic','Black'),
+(124,10,'i3',2024,3.5,135958,69578,'Petrol','Automatic','Red'),
+(125,6,'M5',2019,3.1,192690,37177,'Diesel','Manual','White'),
+(126,1,'X5',2021,3.4,99697,60278,'Diesel','Manual','Red'),
+(127,7,'X5',2021,4.3,44279,96256,'Petrol','Automatic','Black'),
+(128,8,'M5',2021,3.3,88342,108240,'Petrol','Automatic','Grey'),
+(129,4,'X3',2018,3.1,33370,52717,'Petrol','Manual','White'),
+(130,5,'M3',2015,2.0,41514,116902,'Hybrid','Manual','Red');
+
